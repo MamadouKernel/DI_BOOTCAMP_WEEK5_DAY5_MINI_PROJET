6ddc26d5ec03b17e2423910f3c8ef32bf4c2f2ba
Konate Mamadou <120653410+MamadouKernel@users.noreply.github.com>
2023-03-09 10:35:19 +0000

    Add files via upload

---
+++ src/main/java/com/user/management/reactiveapi/ReactiveApiApplication.java
@@ -0,0 +1,13 @@
+package com.user.management.reactiveapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/user/management/reactiveapi/ReactiveApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.user.management.reactiveapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
